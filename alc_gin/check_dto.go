f34a5c00f620b4b3cacd37ef1bf6044d723807cc
michaelzx <mail@zhangxiao.org>
2021-03-10 20:49:48 +0800

    alc_gin，增加CheckDTO4FormMultipart

--- alc_gin/check_dto.go
+++ alc_gin/check_dto.go
@@ -5,6 +5,7 @@ import (
 	"alchemy/alc/alc_lang"
 	"alchemy/alc/alc_validator"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	"reflect"
 	"strings"
@@ -16,6 +17,38 @@ func CheckDTO(context *gin.Context, reqPtr interface{}) {
 		langTag = alc_lang.Cn
 	}
 	if bindErr := context.ShouldBindJSON(reqPtr); bindErr != nil {
+		if vErrs, ok := bindErr.(validator.ValidationErrors); ok {
+			for _, vErr := range vErrs {
+				e := vErr.(validator.FieldError)
+				var errMsg string
+				if langTag == alc_lang.Cn {
+					errMsg = e.Translate(alc_validator.TransCn)
+				} else {
+					errMsg = e.Translate(alc_validator.TransEn)
+				}
+				if filed, ok := reflect.TypeOf(reqPtr).Elem().FieldByName(e.StructField()); ok {
+					var filedName string
+					if langTag == alc_lang.Cn {
+						filedName = filed.Tag.Get("cn")
+					} else {
+						filedName = filed.Tag.Get("en")
+					}
+					errMsg = strings.ReplaceAll(errMsg, e.Field(), filedName)
+				}
+				panic(alc_errs.CommonError(errMsg))
+			}
+		} else {
+			panic(alc_errs.CommonError(bindErr.Error()))
+		}
+	}
+}
+
+func CheckDTO4FormMultipart(context *gin.Context, reqPtr interface{}) {
+	langTag := GetLangTag(context)
+	if langTag == alc_lang.None {
+		langTag = alc_lang.Cn
+	}
+	if bindErr := context.ShouldBindWith(reqPtr, binding.FormMultipart); bindErr != nil {
 		if vErrs, ok := bindErr.(validator.ValidationErrors); ok {
 			for _, vErr := range vErrs {
 				e := vErr.(validator.FieldError)
