5793f70e3ce81141e225f30986fb3c4a75efe2b3
michaelzx <mail@zhangxiao.org>
2021-05-27 15:53:54 +0800

    格式化文件

--- alc_gin/zap.go
+++ alc_gin/zap.go
@@ -1,95 +1,97 @@
 package alc_gin
 
 import (
-	"fmt"
-	"net"
-	"net/http"
-	"net/http/httputil"
-	"os"
-	"runtime/debug"
-	"strings"
-	"time"
+  "fmt"
+  "net"
+  "net/http"
+  "net/http/httputil"
+  "os"
+  "runtime/debug"
+  "strings"
+  "time"
 
-	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
+  "github.com/gin-gonic/gin"
+  "go.uber.org/zap"
 )
+
 // 参考：https://github.com/gin-contrib/zap/blob/master/zap.go
 
 func ZapLogger(logger *zap.Logger, timeFormat string, utc bool) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		start := time.Now()
-		// some evil middlewares modify this values
-		path := c.Request.URL.Path
-		query := c.Request.URL.RawQuery
-		c.Next()
+  // logger = logger.WithOptions(zap.AddCallerSkip(1))
+  return func(c *gin.Context) {
+    start := time.Now()
+    // some evil middlewares modify this values
+    path := c.Request.URL.Path
+    query := c.Request.URL.RawQuery
+    c.Next()
 
-		end := time.Now()
-		latency := end.Sub(start)
-		if utc {
-			end = end.UTC()
-		}
+    end := time.Now()
+    latency := end.Sub(start)
+    if utc {
+      end = end.UTC()
+    }
 
-		if len(c.Errors) > 0 {
-			// Append error field if this is an erroneous request.
-			for _, e := range c.Errors.Errors() {
-				logger.Error(e)
-			}
-		} else {
-			if query != "" {
-				query = "?" + query
-			}
-			logger.Info(fmt.Sprintf("[%s:%d] %s%s", c.Request.Method, c.Writer.Status(), path, query),
-				zap.String("ip", c.ClientIP()),
-				zap.Duration("latency", latency),
-			)
-		}
-	}
+    if len(c.Errors) > 0 {
+      // Append error field if this is an erroneous request.
+      for _, e := range c.Errors.Errors() {
+        logger.Error(e)
+      }
+    } else {
+      if query != "" {
+        query = "?" + query
+      }
+      logger.Debug(fmt.Sprintf("[%s:%d] %s%s", c.Request.Method, c.Writer.Status(), path, query),
+        zap.String("ip", c.ClientIP()),
+        zap.Duration("latency", latency),
+      )
+    }
+  }
 }
 
 func ZapRecovery(logger *zap.Logger, stack bool) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		defer func() {
-			if err := recover(); err != nil {
-				// Check for a broken connection, as it is not really a
-				// condition that warrants a panic stack trace.
-				var brokenPipe bool
-				if ne, ok := err.(*net.OpError); ok {
-					if se, ok := ne.Err.(*os.SyscallError); ok {
-						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
-							brokenPipe = true
-						}
-					}
-				}
+  return func(c *gin.Context) {
+    defer func() {
+      if err := recover(); err != nil {
+        // Check for a broken connection, as it is not really a
+        // condition that warrants a panic stack trace.
+        var brokenPipe bool
+        if ne, ok := err.(*net.OpError); ok {
+          if se, ok := ne.Err.(*os.SyscallError); ok {
+            if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+              brokenPipe = true
+            }
+          }
+        }
 
-				httpRequest, _ := httputil.DumpRequest(c.Request, false)
-				if brokenPipe {
-					logger.Error(c.Request.URL.Path,
-						zap.Any("error", err),
-						zap.String("request", string(httpRequest)),
-					)
-					// If the connection is dead, we can't write a status to it.
-					c.Error(err.(error)) // nolint: errcheck
-					c.Abort()
-					return
-				}
+        httpRequest, _ := httputil.DumpRequest(c.Request, false)
+        if brokenPipe {
+          logger.Error(c.Request.URL.Path,
+            zap.Any("error", err),
+            zap.String("request", string(httpRequest)),
+          )
+          // If the connection is dead, we can't write a status to it.
+          c.Error(err.(error)) // nolint: errcheck
+          c.Abort()
+          return
+        }
 
-				if stack {
-					logger.Error("[Recovery from panic]",
-						zap.Time("time", time.Now()),
-						zap.Any("error", err),
-						zap.String("request", string(httpRequest)),
-						zap.String("stack", string(debug.Stack())),
-					)
-				} else {
-					logger.Error("[Recovery from panic]",
-						zap.Time("time", time.Now()),
-						zap.Any("error", err),
-						zap.String("request", string(httpRequest)),
-					)
-				}
-				c.AbortWithStatus(http.StatusInternalServerError)
-			}
-		}()
-		c.Next()
-	}
+        if stack {
+          logger.Error("[Recovery from panic]",
+            zap.Time("time", time.Now()),
+            zap.Any("error", err),
+            zap.String("request", string(httpRequest)),
+            zap.String("stack", string(debug.Stack())),
+          )
+        } else {
+          logger.Error("[Recovery from panic]",
+            zap.Time("time", time.Now()),
+            zap.Any("error", err),
+            zap.String("request", string(httpRequest)),
+          )
+        }
+        c.AbortWithStatus(http.StatusInternalServerError)
+      }
+    }()
+    c.Next()
+  }
 }
--- alc_logger/logger.go
+++ alc_logger/logger.go
@@ -1,146 +1,146 @@
 package alc_logger
 
 import (
-	"errors"
-	"fmt"
-	"github.com/michaelzx/alc/alc_config"
-	"github.com/michaelzx/alc/alc_fs"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	"log"
-	"path/filepath"
-	"strconv"
-	"strings"
-	"time"
+  "errors"
+  "fmt"
+  "github.com/michaelzx/alc/alc_config"
+  "github.com/michaelzx/alc/alc_fs"
+  "go.uber.org/zap"
+  "go.uber.org/zap/zapcore"
+  "log"
+  "path/filepath"
+  "strconv"
+  "strings"
+  "time"
 )
 
 var (
-	zapLogger *zap.Logger
-	// logDir    = "./app.log"
+  zapLogger *zap.Logger
+  // logDir    = "./app.log"
 )
 
 func New(loggerConfig alc_config.LoggerConfig, skip int) (*zap.Logger, error) {
-	cfg := getZapConfig(loggerConfig.Mode)
-	// 因为我们做了一层包装，所以需要跳过一层caller
-	// 否则，日志的caller位置，始终显示的是当前logger包中的位置
-	// TODO 到底要条几层？
-	callerOption := zap.AddCallerSkip(skip)
-	logger, err := cfg.Build(callerOption)
-	if err != nil {
-		return nil, err
-	}
-	// TODO 初始化的时候，需要做这个吗？
-	defer func() {
-		logger.Sync()
-	}()
-	return logger, nil
+  cfg := getZapConfig(loggerConfig.Mode)
+  // 因为我们做了一层包装，所以需要跳过一层caller
+  // 否则，日志的caller位置，始终显示的是当前logger包中的位置
+  // TODO 到底要条几层？
+  callerOption := zap.AddCallerSkip(skip)
+  logger, err := cfg.Build(callerOption)
+  if err != nil {
+    return nil, err
+  }
+  // TODO 初始化的时候，需要做这个吗？
+  defer func() {
+    logger.Sync()
+  }()
+  return logger, nil
 }
 func Init(loggerConfig alc_config.LoggerConfig) {
-	zapCfg := getZapConfig(loggerConfig.Mode)
-	// 因为我们做了一层包装，所以需要跳过一层caller
-	// 否则，日志的caller位置，始终显示的是当前logger包中的位置
-	// TODO 到底要条几层？
-	callerOption := zap.AddCallerSkip(1)
-	var err error
-	zapLogger, err = zapCfg.Build(callerOption)
-	if err != nil {
-		panic(err)
-	}
-	// TODO 初始化的时候，需要做这个吗？
-	defer func() {
-		zapLogger.Sync()
-	}()
+  zapCfg := getZapConfig(loggerConfig.Mode)
+  // 因为我们做了一层包装，所以需要跳过一层caller
+  // 否则，日志的caller位置，始终显示的是当前logger包中的位置
+  // TODO 到底要条几层？
+  callerOption := zap.AddCallerSkip(1)
+  var err error
+  zapLogger, err = zapCfg.Build(callerOption)
+  if err != nil {
+    panic(err)
+  }
+  // TODO 初始化的时候，需要做这个吗？
+  defer func() {
+    zapLogger.Sync()
+  }()
 }
 func DoSync() {
-	zapLogger.Sync()
+  zapLogger.Sync()
 }
 
 func getZapConfig(mode string) zap.Config {
-	var loggingLevel zapcore.Level
-	var OutputPaths []string
-	var ErrorOutputPaths []string
-	var Encoding string
-	var EncodeLevel zapcore.LevelEncoder
-	var Development bool
-	switch mode {
-	case "prod": // 生产模式 TODO 自动切割文件
-		logPath := filepath.Join(alc_fs.AppPath, "logs")
-		log.Println("log will be written to", logPath)
-		alc_fs.CreateIfNotExist(logPath)
-		Development = false
-		loggingLevel = zap.InfoLevel
-		Encoding = "json"
-		EncodeLevel = zapcore.CapitalLevelEncoder
-		OutputPaths = []string{filepath.Join(logPath, "output.log")}
-		ErrorOutputPaths = []string{filepath.Join(logPath, "errors.log")}
-	case "dev": // 开发模式
-		Development = true
-		loggingLevel = zap.DebugLevel
-		Encoding = "console"
-		// EncodeLevel = zapcore.LowercaseColorLevelEncoder
-		EncodeLevel = func(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
-			s, ok := _levelToLowercaseColorString[level]
-			if !ok {
-				s = _unknownLevelColor.Add(level.String())
-			}
-			enc.AppendString(s)
-		}
-		OutputPaths = []string{"stdout"}
-		ErrorOutputPaths = []string{"stderr"}
-	default:
-		panic(errors.New("日志模式不支持：" + mode))
-	}
-	return zap.Config{
-		Level:             zap.NewAtomicLevelAt(loggingLevel),
-		Development:       Development,
-		DisableCaller:     false,
-		DisableStacktrace: false,
-		Sampling:          nil,
-		Encoding:          Encoding,
-		EncoderConfig: zapcore.EncoderConfig{
-			MessageKey:    "message",
-			LevelKey:      "level",
-			TimeKey:       "time",
-			NameKey:       "name",
-			CallerKey:     "caller",
-			FunctionKey:   "",
-			StacktraceKey: "stacks",
-			LineEnding:    zapcore.DefaultLineEnding,
-			// EncodeLevel:   zapcore.CapitalColorLevelEncoder,
-			EncodeLevel: EncodeLevel,
-			// EncodeTime:    zapcore.ISO8601TimeEncoder,
-			EncodeTime: func(time time.Time, enc zapcore.PrimitiveArrayEncoder) {
-				enc.AppendString(time.Format("06-01-02T15:04:05.000"))
-			},
-			EncodeDuration: zapcore.StringDurationEncoder,
-			EncodeCaller: func(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
-				enc.AppendString(cutStr(caller.FullPath(), 50))
-				funcFullName := caller.Function
-				start := strings.LastIndex(funcFullName, "/")
-				enc.AppendString(cutStr(funcFullName[start+1:], 30))
-			},
-			EncodeName:       zapcore.FullNameEncoder,
-			ConsoleSeparator: "  ",
-		},
-		OutputPaths:      OutputPaths,
-		ErrorOutputPaths: ErrorOutputPaths,
-		InitialFields:    nil,
-	}
+  var loggingLevel zapcore.Level
+  var OutputPaths []string
+  var ErrorOutputPaths []string
+  var Encoding string
+  var EncodeLevel zapcore.LevelEncoder
+  var Development bool
+  switch mode {
+  case "prod": // 生产模式 TODO 自动切割文件
+    logPath := filepath.Join(alc_fs.AppPath, "logs")
+    log.Println("log will be written to", logPath)
+    alc_fs.CreateIfNotExist(logPath)
+    Development = false
+    loggingLevel = zap.InfoLevel
+    Encoding = "json"
+    EncodeLevel = zapcore.CapitalLevelEncoder
+    OutputPaths = []string{filepath.Join(logPath, "output.log")}
+    ErrorOutputPaths = []string{filepath.Join(logPath, "errors.log")}
+  case "dev": // 开发模式
+    Development = true
+    loggingLevel = zap.DebugLevel
+    Encoding = "console"
+    // EncodeLevel = zapcore.LowercaseColorLevelEncoder
+    EncodeLevel = func(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+      s, ok := _levelToLowercaseColorString[level]
+      if !ok {
+        s = _unknownLevelColor.Add(level.String())
+      }
+      enc.AppendString(s)
+    }
+    OutputPaths = []string{"stdout"}
+    ErrorOutputPaths = []string{"stderr"}
+  default:
+    panic(errors.New("日志模式不支持：" + mode))
+  }
+  return zap.Config{
+    Level:             zap.NewAtomicLevelAt(loggingLevel),
+    Development:       Development,
+    DisableCaller:     false,
+    DisableStacktrace: false,
+    Sampling:          nil,
+    Encoding:          Encoding,
+    EncoderConfig: zapcore.EncoderConfig{
+      MessageKey:    "message",
+      LevelKey:      "level",
+      TimeKey:       "time",
+      NameKey:       "name",
+      CallerKey:     "caller",
+      FunctionKey:   "",
+      StacktraceKey: "stacks",
+      LineEnding:    zapcore.DefaultLineEnding,
+      // EncodeLevel:   zapcore.CapitalColorLevelEncoder,
+      EncodeLevel: EncodeLevel,
+      // EncodeTime:    zapcore.ISO8601TimeEncoder,
+      EncodeTime: func(time time.Time, enc zapcore.PrimitiveArrayEncoder) {
+        enc.AppendString(time.Format("06-01-02T15:04:05.000"))
+      },
+      EncodeDuration: zapcore.StringDurationEncoder,
+      EncodeCaller: func(caller zapcore.EntryCaller, enc zapcore.PrimitiveArrayEncoder) {
+        enc.AppendString(cutStr(caller.FullPath(), 50))
+        funcFullName := caller.Function
+        start := strings.LastIndex(funcFullName, "/")
+        enc.AppendString(cutStr(funcFullName[start+1:], 30))
+      },
+      EncodeName:       zapcore.FullNameEncoder,
+      ConsoleSeparator: "  ",
+    },
+    OutputPaths:      OutputPaths,
+    ErrorOutputPaths: ErrorOutputPaths,
+    InitialFields:    nil,
+  }
 }
 func cutStr(fullPath string, maxLen int) string {
-	fullPathLen := len(fullPath)
-	switch {
-	case fullPathLen == maxLen:
-		return fullPath
-	case fullPathLen < maxLen:
-		lenStr := strconv.Itoa(maxLen)
-		return fmt.Sprintf("%"+lenStr+"s", fullPath)
-	case fullPathLen > maxLen:
-		start := fullPathLen - (maxLen - 2)
-		if start < 0 {
-			start = 0
-		}
-		return ".." + fullPath[start:]
-	}
-	return ""
+  fullPathLen := len(fullPath)
+  switch {
+  case fullPathLen == maxLen:
+    return fullPath
+  case fullPathLen < maxLen:
+    lenStr := strconv.Itoa(maxLen)
+    return fmt.Sprintf("%"+lenStr+"s", fullPath)
+  case fullPathLen > maxLen:
+    start := fullPathLen - (maxLen - 2)
+    if start < 0 {
+      start = 0
+    }
+    return ".." + fullPath[start:]
+  }
+  return ""
 }
