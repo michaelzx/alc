f392bebf58c41707dc2c8869ca7b2c7d92172932
michaelzx <mail@zhangxiao.org>
2022-03-12 16:32:22 +0800

    alc_result commerr+prefix+suffix

--- alc_result/errcomm.go
+++ alc_result/errcomm.go
@@ -6,6 +6,16 @@ func (e *CommonErr) Error() string {
 	return e.Msg
 }
 
+func (e *CommonErr) Prefix(s string) *CommonErr {
+	e.Msg = s + e.Msg
+	return e
+}
+
+func (e *CommonErr) Suffix(s string) *CommonErr {
+	e.Msg = e.Msg + s
+	return e
+}
+
 func NewCommonErr(code int, msg string) *CommonErr {
 	return &CommonErr{
 		code,
